a384670bccb5331504e19d8b5e812b5888545f02
Ankush <ankushrawat950@gmail.com>
2019-01-27 15:43:04 +0530

    DashBoard added

--- app/src/main/java/farm/gecdevelopers/com/farm/activity/admin/DashBoardActivity.java
+++ app/src/main/java/farm/gecdevelopers/com/farm/activity/admin/DashBoardActivity.java
@@ -2,14 +2,27 @@ package farm.gecdevelopers.com.farm.activity.admin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import farm.gecdevelopers.com.farm.R;
 
 public class DashBoardActivity extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+
+
     }
+
+
+
+
+
+
 }
